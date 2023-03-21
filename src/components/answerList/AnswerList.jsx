a8141893c659a8482fb8194d6b1df2f05f022fec
Dat Huynh <huynhdat2703@gmail.com>
2023-03-21 21:28:59 +0700

    init layout

---
+++ src/components/answerList/AnswerList.jsx
@@ -0,0 +1,11 @@
+import Answer from "../answer/Answer";
+
+function AnswerList() {
+    return (
+        <>
+            <h3>AnswerList</h3>
+            <Answer />
+        </>
+    );
+}
+export default AnswerList;
